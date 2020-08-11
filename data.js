d19692bc80fdb30ff9c443ee6f77bc86596400be
Ghandhy <jghandhy01@rbi.com>
2020-08-10 22:04:14 -0400

    project week 3

--- data.js
+++ data.js
@@ -1,6 +1,11 @@
 let responses = [];
 const userResponsesSection = document.querySelector('#user-responses');
 
+const nameSelect = document.querySelector('#name');
+const favouriteSnackSelect = document.querySelector('#favorite-snack');
+const favouriteCountrySelect = document.querySelector('#favourite-country');
+const searchInput = document.querySelector('#search');
+
 const fetchUserResponses = async () => {
   const response = await fetch(
     'https://cors-anywhere.herokuapp.com/https://docs.google.com/spreadsheets/d/e/2PACX-1vT-qq72MCgYI2Gmt6MCkiqYejVbxvSfuG9oQKBcj-6BIwJSVuN23yTSUKa-f5xoiaDl7FyA8nBNPVuO/pub?output=csv'
@@ -9,16 +14,6 @@ const fetchUserResponses = async () => {
   const results = Papa.parse(data, { header: true });
   responses = results.data;
 };
-
-// During vacation time, what has been your favourite travel destination to-date?: "Don’t have favorites "
-// If you could only go to one of the following countries, which one would you choose!: "Singapore"
-// In 2-3 sentences, please describe your most memorable work experience at RBI. (Be creative!): "Wearing a suit on the first day of work, also we had an AEM and everyone looking at me. "
-// "On a scale of 1-5, how likely are you to return to that travel destination? ": "3"
-// "Over the brand's 70 year history, which Burger King logo has been your favorite? Please upload an example of your favorite logo. ": "https://drive.google.com/open?id=1Sj-NaWcY4WHecXLXWjdepDXaLS7cn93E"
-// Timestamp: "08/08/2020 11:24:11"
-// What brand do you interact with most as part of your job?: "Tim Hortons"
-// What is your first and last name?: "Yash Maha"
-// "Which brand carries your favorite indulgent snack? ": "Burger King"
 
 const renderUserResponse = userResponse => {
   console.log(userResponse);
@@ -72,3 +67,84 @@ const fetchAndShowResponses = async () => {
 };
 
 fetchAndShowResponses();
+
+const teamFilter = team => {
+  const selectedFavouriteSnack = favouriteSnackSelect.value;
+  console.log(selectedFavouriteSnack);
+  const selectedFavouriteCountry = favouriteCountrySelect.value;
+  const selectedName = nameSelect.value;
+  const searchTerm = searchInput.value.toLowerCase();
+  console.log({
+    teamFavourite: team['Which brand carries your favorite indulgent snack? '],
+    selectedFavouriteSnack,
+  });
+  return (
+    (selectedFavouriteSnack === 'all' ||
+      selectedFavouriteSnack
+        .toLowerCase()
+        .includes(
+          team[
+            'Which brand carries your favorite indulgent snack? '
+          ].toLowerCase()
+        )) &&
+    (selectedFavouriteCountry === 'all' ||
+      team[
+        'If you could only go to one of the following countries, which one would you choose!'
+      ] === selectedFavouriteCountry) &&
+    (selectedName === 'all' ||
+      team['What is your first and last name?'] === selectedName) &&
+    team['What is your first and last name?'].toLowerCase().includes(searchTerm)
+  );
+};
+
+const handleFilterInput = () => {
+  const filteredteamFilter = responses.filter(teamFilter);
+  userResponsesSection.innerHTML = filteredteamFilter
+    .map(renderUserResponse)
+    .join('');
+};
+
+favouriteSnackSelect.addEventListener('input', handleFilterInput);
+nameSelect.addEventListener('input', handleFilterInput);
+favouriteCountrySelect.addEventListener('input', handleFilterInput);
+searchInput.addEventListener('input', handleFilterInput);
+
+const question = 'Which country would you prefer to travel for vacation?';
+
+const votes = {
+  France: 0,
+  Germany: 0,
+  Singapore: 0,
+  Thailand: 0,
+};
+
+responses.forEach(response => {
+  votes[response[question]] += 1;
+});
+
+const franceSpan = document.querySelector('#france-votes');
+const germanySpan = document.querySelector('#germany-votes');
+const singaporeSpan = document.querySelector('#singapore-votes');
+const thailandSpan = document.querySelector('#thailand-votes');
+
+franceSpan.textContent = votes.France;
+germanySpan.textContent = votes.Germany;
+singaporeSpan.textContent = votes.Singapore;
+thailandSpan.textContent = votes.Thailand;
+
+new Chart('pie-chart', {
+  type: 'pie',
+  data: {
+    datasets: [
+      {
+        data: Object.values(votes),
+        backgroundColor: [
+          'rgb(195,226,228)',
+          'rgb(170,207,213)',
+          'rgb(104,184,193)',
+        ],
+      },
+    ],
+    labels: Object.keys(votes),
+  },
+});
